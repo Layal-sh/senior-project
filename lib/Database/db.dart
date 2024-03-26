557b01b552189ba9f1c21e2f145fad33d3593987
AliSinno <alisinno26@gmail.com>
2024-03-26 09:06:41 +0200

    alter table

--- lib/Database/db.dart
+++ lib/Database/db.dart
@@ -27,21 +27,14 @@ class DBHelper {
     String DbPath = await getDatabasesPath();
     String path = join(DbPath, 'SugarSense.db');
     Database database = await openDatabase(path,
-        onCreate: _onCreate, version: 22, onUpgrade: _onUpgrade);
+        onCreate: _onCreate, version: 23, onUpgrade: _onUpgrade);
     return database;
   }
 
   _onUpgrade(Database db, int oldVersion, int newVersion) async {
     if (oldVersion < newVersion) {
-      //     await db.execute('DROP TABLE IF EXISTS Entry');
-      //     await db.execute('''
-      // CREATE TABLE "Entry"(
-      //   entryId INTEGER PRIMARY KEY AUTOINCREMENT,
-      //   glucoseLevel REAL NOT NULL,
-      //   insulinDosage INTEGER NULL,
-      //   entryDate TEXT NOT NULL
-      // );
-      // ''');
+      await db.execute(
+          '''ALTER TABLE "Meals" ADD COLUMN frequency INTEGER NULL;''');
     }
   }
 
