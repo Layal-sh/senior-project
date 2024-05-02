f38888c99341c96521590598d52c62fcf8fc311a
Feetuss <alisinno26@gmail.com>
2024-05-02 23:29:46 +0300

    removed the birthday stuff from the login

--- lib/login/signup/login.dart
+++ lib/login/signup/login.dart
@@ -114,14 +114,6 @@ class _LoginState extends State<Login> {
     }
 
     if (id != pid_) {
-      final birthday =
-          await http.get(Uri.parse("http://$localhost:8000/getBirthday/$id"));
-      if (birthday.statusCode == 200) {
-        Map<String, dynamic> birthdayDetails = jsonDecode(birthday.body);
-        birthDate_ = birthdayDetails['birthday'];
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        await prefs.setString('birthDate', birthDate_);
-      }
       final response = await http
           .post(
             Uri.parse('http://$localhost:8000/getUserDetails'),
@@ -468,23 +460,6 @@ class _LoginState extends State<Login> {
                                 );
                               } else {
                                 try {
-                                  //birthday stuff
-                                  if (birthDate_ != "" && selectedPlan_ == -1) {
-                                    DateTime birthDate = DateTime.parse(
-                                        birthDate_); // assuming user.birthDate is in 'yyyy-MM-dd' format
-                                    DateTime twentyTwoYearsLater =
-                                        birthDate.add(Duration(
-                                            days: 22 *
-                                                365)); // this is a rough calculation, not accounting for leap years
-
-                                    if (twentyTwoYearsLater
-                                        .isAfter(DateTime.now())) {
-                                      //WE HAVE TO GO TO THE MEMBERSHIPP PAGEESSSOUYIGHFUIHBKJDHBUYDS
-                                      // The birthday after 22 years is in the future
-                                      final response = await http.get(Uri.parse(
-                                          "http:$localhost:8000/updateSubscription/$pid_/0"));
-                                    }
-                                  }
                                   //server authentication
                                   final response = await http
                                       .post(
